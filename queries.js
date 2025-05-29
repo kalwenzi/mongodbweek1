b15bc575eaa61a1547abb54312bd3de27c5f440f
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-29 08:52:29 +0300

    sort in ascending order by price

--- queries.js
+++ queries.js
@@ -188,3 +188,7 @@ db.books.find({$and:[{in_stock:true},{published_year:{$gt:2010}}]})
 
 //Use projection to return only the title, author, and price fields in your queries
 db.books.find({$and:[{in_stock:true},{published_year:{$gt:2010}}]},{_id:false,title:1,author:1,price:1})
+
+//Implement sorting to display books by price (both ascending and descending)
+//Ascending order
+db.books.find({},{_id:false,title:1,author:1,price:1}).sort({price:1})
