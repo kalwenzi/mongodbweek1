9216650a041c36b44203e66042094050880b636d
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2025-05-29 08:48:48 +0300

    returning specific fields

--- queries.js
+++ queries.js
@@ -185,3 +185,6 @@ db.books.find({ title: "The Midnight Library" })
 
 //find books that are both in stock and published after 2010
 db.books.find({$and:[{in_stock:true},{published_year:{$gt:2010}}]})
+
+//Use projection to return only the title, author, and price fields in your queries
+db.books.find({$and:[{in_stock:true},{published_year:{$gt:2010}}]},{_id:false,title:1,author:1,price:1})
